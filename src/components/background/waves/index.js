d8db407cc4c87f53f3a1a0fd2b24f907dcfac35a
David <ddropping@gmail.com>
2020-08-07 20:11:10 -0700

    Create background waves animation

--- src/components/background/waves/index.js
+++ src/components/background/waves/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 
 import Waves1 from "./Wave1"
 import Waves2 from "./Wave2"
@@ -6,15 +7,19 @@ import Waves3 from "./Wave3"
 import Waves4 from "./Wave4"
 import Waves5 from "./Wave5"
 
+const Container = styled.div`
+  position: relative;
+`
+
 const index = () => {
   return (
-    <div>
+    <Container>
       <Waves5 />
       <Waves4 />
       <Waves3 />
       <Waves2 />
       <Waves1 />
-    </div>
+    </Container>
   )
 }
 
