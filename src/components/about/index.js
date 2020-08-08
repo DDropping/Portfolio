6e67bf861c0cb1287fe72053562a9881a6fdd092
David <ddropping@gmail.com>
2020-08-07 22:03:05 -0700

    Create about component

---
+++ src/components/about/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  margin: 140px 0;
+`
+
+const index = () => {
+  return <Container>Hello, my name is David</Container>
+}
+
+export default index
