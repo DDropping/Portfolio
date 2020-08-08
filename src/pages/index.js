9b5124198f572a81208c51c1e75c55ee7d9706c1
David <ddropping@gmail.com>
2020-08-07 22:06:28 -0700

    Update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import About from "../components/about"
+import Skills from "../components/skills"
 import Footer from "../components/footer"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div>hello world</div>
+    <About />
+    <Skills />
     <Footer />
   </Layout>
 )
