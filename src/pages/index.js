53a8952030425876971a530447388a4744eff9ba
David <ddropping@gmail.com>
2020-08-07 20:12:40 -0700

    Update index with header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
 import React from "react"
-//import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import Waves from "../components/background/waves"
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Waves />
   </Layout>
 )
 
