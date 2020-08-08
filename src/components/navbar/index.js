357b4e47eb047f2f08b36ae1059a25a3e9326d93
David <ddropping@gmail.com>
2020-08-07 20:12:12 -0700

    Create navbar component

---
+++ src/components/navbar/index.js
@@ -0,0 +1,49 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+import navbarLinks from "../../constants/navbarLinks"
+import HomeIcon from "./HomeIcon"
+
+const Ul = styled.ul`
+  width: 100%;
+  min-height: 50px; /* size of home button */
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+`
+
+const Li = styled.li`
+  margin: 10px;
+  color: #000000bf;
+  border-bottom: 2px solid #b8f4ec;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    color: #000000;
+    border-bottom: 2px solid #000000;
+  }
+`
+
+const index = () => {
+  return (
+    <Ul>
+      <Link to="/">
+        <HomeIcon />
+      </Link>
+      {navbarLinks.map((item, index) => {
+        return (
+          <Link to={item.link} style={{ textDecoration: "none" }}>
+            <Li key={index}>
+              {item.icon}
+              {item.title}
+            </Li>
+          </Link>
+        )
+      })}
+    </Ul>
+  )
+}
+
+export default index
