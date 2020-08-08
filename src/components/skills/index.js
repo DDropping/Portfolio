84b97a18f1a143ab92ec2efef50ff76009af601d
David <ddropping@gmail.com>
2020-08-07 22:05:30 -0700

    Create skills component

---
+++ src/components/skills/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import TealTop from "../background/waves/tealTop"
+
+const index = () => {
+  return (
+    <div>
+      <TealTop />
+      skills section
+    </div>
+  )
+}
+
+export default index
