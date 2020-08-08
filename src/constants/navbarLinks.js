6792125bfe4e29449d0ab24529f7fda467ac9914
David <ddropping@gmail.com>
2020-08-07 20:08:59 -0700

    Create navbar links constants

---
+++ src/constants/navbarLinks.js
@@ -0,0 +1,36 @@
+import React from "react"
+import {
+  CodeOutlined,
+  GithubOutlined,
+  LinkedinOutlined,
+  CopyOutlined,
+  ContactsOutlined,
+} from "@ant-design/icons"
+
+export default [
+  {
+    title: "Projects",
+    icon: <CodeOutlined />,
+    link: "/page-2",
+  },
+  {
+    title: "Github",
+    icon: <GithubOutlined />,
+    link: "/page-2",
+  },
+  {
+    title: "LinkedIn",
+    icon: <LinkedinOutlined />,
+    link: "/page-2",
+  },
+  {
+    title: "Resume",
+    icon: <CopyOutlined />,
+    link: "/page-2",
+  },
+  {
+    title: "Contact",
+    icon: <ContactsOutlined />,
+    link: "/page-2",
+  },
+]
