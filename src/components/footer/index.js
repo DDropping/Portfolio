8570ae7c37e59de5b39d664381eed759ca231d2c
David <ddropping@gmail.com>
2020-08-08 23:52:25 -0700

    Update footer component with contact

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,17 +1,46 @@
 import React from "react"
 import styled from "styled-components"
+import { Link } from "gatsby"
 
 import GreenTop from "../background/waves/greenTop.js"
+import contact from "../../constants/contact"
 
 const Container = styled.div`
   background-color: #aff0c2;
+`
+const ContactContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 1rem;
+`
+
+const ContactItem = styled.div`
+  margin: 0 25px;
+`
+
+const Note = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1rem;
 `
 
 const index = () => {
   return (
     <Container>
       <GreenTop />
-      footer
+      <ContactContainer>
+        {contact.map((item, index) => {
+          return (
+            <ContactItem key={index}>
+              <a href={item.link} target="_blank" style={{ color: "black" }}>
+                {item.icon}
+                {item.title}
+              </a>
+            </ContactItem>
+          )
+        })}
+      </ContactContainer>
+      <Note>For fastest response, email me at ddropping@gmail.com</Note>
     </Container>
   )
 }
