1e62bea7f2eb81484014af49354ff382f0b8bd7e
David <ddropping@gmail.com>
2020-08-08 20:44:51 -0700

    Create projects folder

---
+++ src/components/projects/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  margin: 140px 0;
+`
+
+const index = () => {
+  return (
+    <Container>
+      projects
+      <div>projects</div>
+    </Container>
+  )
+}
+
+export default index
