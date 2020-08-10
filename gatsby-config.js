5872af4616da2e7ef2940554438e96e7ef751da0
David <ddropping@gmail.com>
2020-08-09 18:32:55 -0700

    Fix styled-components not loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     "gatsby-plugin-antd",
+    `gatsby-plugin-styled-components`,
     {
       resolve: "gatsby-plugin-anchor-links",
       options: {
